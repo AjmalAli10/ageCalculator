cf49f8625868c7141b1df5d43182113ca459fe0f
Ajmal Ali <officialajmalali10@gmail.com>
2023-02-05 19:02:45 +0530

    pull

--- App.js
+++ App.js
@@ -3,8 +3,41 @@ class User{
         this.birthdate = birthdate;
     }
     get age(){
-        let todayYear = new Date().getFullYear();
-        return todayYear - this.birthdate.getFullYear()
+        let currentYear = new Date().getFullYear();
+        let birthYear = this.birthdate.getFullYear();
+
+        let completedYears = currentYear - birthYear;
+
+        let currenMonth = new Date().getMonth() + 1;
+        let birthMonth = this.birthdate.getMonth() + 1;
+
+        let completedMonths = currenMonth - birthMonth
+        
+
+        // let currentDate = new Date().getDate();
+        // let birthDate = this.birthdate.getDate();
+
+        // let completedDays = currentDate - birthDate;
+
+        // const getDays = (year, month) => {
+        //     return new Date(year, month, 0).getDate();
+        // };
+        // const totalDaysInBirthMonth = getDays(birthYear, birthMonth);
+
+        if(completedMonths < 0){
+            return completedYears =  completedYears - 1
+        }else{
+            return completedYears = currentYear - birthYear;
+        }
+        // console.log(birthYear, birthMonth)
+
+        // console.log(totalDaysInBirthMonth)
+        // if(currentDate < 0){
+        //     completedDays = completedDays + totalDaysInBirthMonth;
+        // }
+        // let totalMonths = completedYears + (completedMonths) + completedDays;
+        // console.log(totalMonths)
+
     }
 }
 const root = document.getElementById("root");
@@ -15,15 +48,15 @@ input.setAttribute("class", "input")
 root.append(input)
 
 input.addEventListener("input", (e)=>{
-    date = new Date(e.currentTarget.value);
-    const dateBith = new User(date);
-    console.log(dateBith.age)
+    date = new Date(e.target.value);
+    const dateBirth = new User(date);
+    console.log(dateBirth.age)
     let h2 = root.querySelector("h2");
     if (h2) {
         h2.remove();
     }
     let ele = document.createElement("h2");
     ele.setAttribute("class", "h2")
-    ele.innerText = dateBith.age  + " years old 😃";
+    ele.innerText = dateBirth.age  + " years old 😃";
     root.append(ele)
 })
